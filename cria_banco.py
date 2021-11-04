7cc95f71e15b5aeb53ae423a944abd06d9f87afe
thiago <kebagi7883@edmondpt.com>
2021-11-03 21:06:03 -0300

    integracao

---
+++ cria_banco.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+connection = sqlite3.connect('banco.db')
+cursor = connection.cursor()
+
+
+cria_tabela = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRYMARY KEY,\
+     nome text, estrelas real, diaria real, cidade text )"
+
+cria_hotel = "INSERT INTO hoteis VALUES ('alpha', 'alphahotel', 4.3, 344.22, 'Sao Paulo')"
+
+cursor.execute(cria_tabela)
+cursor.execute(cria_hotel)
+
+connection.commit()
+connection.close()
---
+++ resources/hotel.py
@@ -0,0 +1,59 @@
+from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
+
+
+
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hotels': [hotel.json() for hotel in HotelModel.query.all() ]}
+
+
+class Hotel(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+
+    def get(self,hotel_id): 
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            return hotel.json()
+        return {"message": "hotel not found"}, 404
+
+
+ 
+
+
+    def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message":"hotel id {} already exists.".format(hotel_id)}, 400
+        # elif hotel_id != int:
+        #     return {"message": "voce digitou: {}, campo so aceita numeros ".format(hotel_id.upper())}
+
+        dados = Hotel.argumentos.parse_args()
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json()
+
+
+    def put(self,hotel_id):
+        dados = Hotel.argumentos.parse_args()
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json(), 201
+
+
+    def delete(self, hotel_id):
+        hotel  = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            hotel.delete_hotel()
+            return {'Message':'Hotel :{} deleted.'.format(hotel_id.upper())}
+        return {"message": "hotel not found."}, 404
